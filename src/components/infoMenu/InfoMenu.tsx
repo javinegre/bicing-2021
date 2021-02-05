89fc449fb03a7c02733c606a0a175fb1b3aaea3e
Javier Negre <javi.negre@gmail.com>
2021-02-05 18:03:00 +0100

    Improved style for InfoMenu

--- src/components/infoMenu/InfoMenu.tsx
+++ src/components/infoMenu/InfoMenu.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
+import StationStatusBar from '../../stationStatusBar/stationStatusBar';
+import Icon from '../ui/icon/icon';
+import Spacer from '../ui/spacer/spacer';
 import storeHooks from '../../store/hooks';
 import { sortByDistance, splitStationListByDistance } from './helpers';
+import appConfig from '../../config';
 
 import './InfoMenu.css';
 
@@ -36,6 +40,8 @@ const InfoMenu: React.FunctionComponent = () => {
 
   const showAboutMenu: () => void = () => toggleAboutMenu(true);
 
+  const iconColor = appConfig.infoMenuIconColor;
+
   return (
     <div className="InfoMenu-wrapper order-1">
       <div
@@ -43,51 +49,78 @@ const InfoMenu: React.FunctionComponent = () => {
         onClick={hideMenu}
       />
       <aside className={`InfoMenu ${getMenuVisibilityClassName()}`}>
-        <div className="InfoMenu-section--sticky">
-          {stationData && (
-            <div>
-              {stationData.name}
-              <br />
-              {stationData.bikes}: ({stationData.mechanical} +{' '}
-              {stationData.electrical})
-              <br />
-              {stationData.docks}
+        {stationData && (
+          <div className="flex-grow-0 px-4 pt-3 pb-5">
+            <div className="flex justify-between">
+              <div className="flex items-center">
+                <div className="flex items-center">
+                  <div className="text-3xl font-bold">{stationData.bikes}</div>
+                  <Icon name="bike" color={iconColor} />
+                </div>
+
+                <Spacer x={8} />
+
+                <div className="flex items-center">
+                  <div>{stationData.mechanical}</div>
+                  <Icon name="gears" color={iconColor} size={12} />
+                </div>
+
+                <Spacer x={6} />
+
+                <div className="flex items-center">
+                  <div>{stationData.electrical}</div>
+                  <Icon name="bolt" color={iconColor} size={12} />
+                </div>
+              </div>
+
+              <div className="flex items-center">
+                <Icon name="parking" color={iconColor} />
+                <div className="text-3xl font-bold">{stationData.docks}</div>
+              </div>
             </div>
-          )}
-        </div>
-        <hr />
-        <div className="InfoMenu-section--scrollable">
-          <div>Closest Station</div>
+
+            <StationStatusBar
+              stationData={stationData}
+              className="mt-0.5 mb-1.5"
+            />
+
+            <div>{stationData.name}</div>
+          </div>
+        )}
+        <div className="InfoMenu-section--scrollable flex-grow-1 px-4 py-3">
+          <div className="pb-4 font-light">Closest Station</div>
           {closestStations.sort(sortByDistance).map((station) => (
-            <span key={station.id}>
-              <span
-                onClick={(): void => {
-                  selectStation(station.id);
-                }}
-              >
-                {station.name}
-              </span>
-              <br />
-            </span>
+            <div
+              key={station.id}
+              className="pb-6"
+              onClick={(): void => {
+                selectStation(station.id);
+              }}
+            >
+              <StationStatusBar stationData={station} height={1} />
+              <div className="text-xs font-extralight">{station.name}</div>
+            </div>
           ))}
-          <div>Other Stations</div>
+          <div className="pb-4 font-light">Other Stations</div>
           {farthestStations.sort(sortByDistance).map((station) => (
-            <span key={station.id}>
-              <span
-                onClick={(): void => {
-                  selectStation(station.id);
-                }}
-              >
-                {station.name}
-              </span>
-              <br />
-            </span>
+            <div
+              key={station.id}
+              className="pb-6"
+              onClick={(): void => {
+                selectStation(station.id);
+              }}
+            >
+              <StationStatusBar stationData={station} height={1} />
+              <div className="text-xs font-extralight">{station.name}</div>
+            </div>
           ))}
         </div>
-        <div className="InfoMenu-section--sticky" onClick={showAboutMenu}>
-          <span role="img" aria-label="Info">
-            ℹ️
-          </span>
+        <div
+          className="flex-grow-0 flex items-center px-4 py-3"
+          onClick={showAboutMenu}
+        >
+          <Icon name="info" color={iconColor} />
+          <Spacer x={4} />
           Info about this app
         </div>
       </aside>
