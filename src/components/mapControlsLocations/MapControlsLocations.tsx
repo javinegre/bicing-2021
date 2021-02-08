55c4f835c0e84194024ece0ecafe6ff4bb6057cc
Javier Negre <javi.negre@gmail.com>
2021-02-08 16:30:15 +0100

    Add geolocation option in maps buttons

--- src/components/mapControlsLocations/MapControlsLocations.tsx
+++ src/components/mapControlsLocations/MapControlsLocations.tsx
@@ -3,31 +3,65 @@ import React from 'react';
 import Button from '../ui/button/Button';
 import Icon from '../ui/icon/icon';
 import Spacer from '../ui/spacer/spacer';
+import storeHooks from '../../store/hooks';
 
-const MapControlsLocations: React.FunctionComponent = () => (
-  <>
-    <Button onClick={(): void => {}} color="lightgray" size="lg">
-      <Icon name="user-location" color="black" size={24} />
-    </Button>
+const MapControlsLocations: React.FunctionComponent = () => {
+  const { setMapCenter, setUserLocation } = storeHooks.useStoreActions(
+    (actions) => ({
+      setMapCenter: actions.map.setMapCenter,
+      setUserLocation: actions.map.setUserLocation,
+    }),
+  );
 
-    <Spacer y={6} />
+  const updateUserLocation: (position: GeolocationPosition) => void = (
+    position,
+  ) => {
+    const userLocation = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude,
+    };
+    setMapCenter(userLocation);
+    setUserLocation(userLocation);
+  };
 
-    <Button color="lightgray" size="sm" onClick={(): void => {}}>
-      <Icon name="home" color="black" size={16} />
-    </Button>
+  const geoLocate: () => void = () => {
+    const geolocationOptions: PositionOptions = {
+      timeout: 10000,
+      maximumAge: 30000,
+    };
 
-    <Spacer y={6} />
+    navigator.geolocation.getCurrentPosition(
+      updateUserLocation,
+      undefined,
+      geolocationOptions,
+    );
+  };
 
-    <Button color="lightgray" size="sm" onClick={(): void => {}}>
-      <Icon name="briefcase" color="black" size={16} />
-    </Button>
+  return (
+    <>
+      <Button onClick={geoLocate} color="lightgray" size="lg">
+        <Icon name="user-location" color="black" size={24} />
+      </Button>
 
-    <Spacer y={6} />
+      <Spacer y={6} />
 
-    <Button color="lightgray" size="sm" onClick={(): void => {}}>
-      <Icon name="star" color="black" size={16} />
-    </Button>
-  </>
-);
+      <Button color="lightgray" size="sm" onClick={(): void => {}}>
+        <Icon name="home" color="black" size={16} />
+      </Button>
+
+      <Spacer y={6} />
+
+      <Button color="lightgray" size="sm" onClick={(): void => {}}>
+        <Icon name="briefcase" color="black" size={16} />
+      </Button>
+
+      <Spacer y={6} />
+
+      <Button color="lightgray" size="sm" onClick={(): void => {}}>
+        <Icon name="star" color="black" size={16} />
+      </Button>
+    </>
+  );
+};
 
 export default MapControlsLocations;
