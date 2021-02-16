6a8af03c9747ef3722c21b596d96f6750a8eea20
Javier Negre <javi.negre@gmail.com>
2021-02-16 18:19:00 +0100

    Update Api version 1.0 -> 1.1

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ app.use(
 
 app.use(express.static(path.join(__dirname, '/build')));
 
-app.get('/api/v1.0/station-info', async (req, res) => {
+app.get('/api/v1.1/station-info', async (req, res) => {
   const resData = await Api().getStationInfo();
 
   res.setHeader('Content-Type', 'application/json');
   res.send(JSON.stringify(resData, null, 0));
 });
 
-app.get('/api/v1.0/station-status', async (req, res) => {
+app.get('/api/v1.1/station-status', async (req, res) => {
   const resData = await Api().getStationStatus();
 
   res.setHeader('Content-Type', 'application/json');
